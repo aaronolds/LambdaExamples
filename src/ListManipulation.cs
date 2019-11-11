6ee38ffc57432242cd70f80e6a6f134cb7232cef
Aaron P. Olds <aaron.olds@gmail.com>
2019-11-11 09:36:31 -0500

    Added new examples

---
+++ src/ListManipulation.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Linq;
+using LambdaExamples;
+
+namespace LambdaExamples
+{
+    public class ListManipulation
+    {
+        public ListManipulation()
+        {
+        }
+
+        public List<Shoe> RemoveBrand(string brand)
+        {
+            var theList = SharedKernel.ShoeList();
+
+            theList.RemoveAll(x => x.Brand == brand);
+            return theList;
+        }
+
+
+        public List<Shoe> RemoveWhereInList(List<RemoveMe> removeMe)
+        {
+            var theList = SharedKernel.ShoeList();
+
+            theList.RemoveAll(x => removeMe.Any(y => y.Brand == x.Brand && y.Category == x.Category));
+            return theList;
+        }
+
+        public List<Shoe> RemoveWhereNotInList(List<RemoveMe> removeMe)
+        {
+            var theList = SharedKernel.ShoeList();
+
+            theList.RemoveAll(x => !removeMe.Any(y => y.Brand == x.Brand && y.Category == x.Category));
+            return theList;
+        }
+
+        public List<Shoe> GetTopNShoes(int n)
+        {
+            var theList = SharedKernel.ShoeList();
+
+            return theList.Take(n).ToList();
+        }
+    }
+
+    public class RemoveMe
+    {
+        public string Category { get; }
+        public string Brand { get; }
+        public RemoveMe(string brand, string category)
+        {
+            Brand = brand;
+            Category = category;
+        }
+    }
+}
---
+++ test/ListManipulationTests.cs
@@ -0,0 +1,49 @@
+using Shouldly;
+using Xunit;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LambdaExamplesTests
+{
+    public class ListManipulationTests
+    {
+        [Fact]
+        public void RemoveBrandTest()
+        {
+            var x = new LambdaExamples.ListManipulation();
+            var list = x.RemoveBrand("Brooks");
+            list.ShouldNotBeEmpty();
+            list.Count(x => x.Brand == "Brooks").ShouldBe(0);
+        }
+
+        [Fact]
+        public void RemoveBrandListTest()
+        {
+            var x = new LambdaExamples.ListManipulation();
+            var list = x.RemoveWhereInList(new List<LambdaExamples.RemoveMe> { new LambdaExamples.RemoveMe("Nike", "Running") });
+            list.ShouldNotBeEmpty();
+            list.Count(x => x.Brand == "Nike" && x.Category == "Running").ShouldBe(0);
+        }
+
+        [Fact]
+        public void RemoveBrandNotListTest()
+        {
+            var x = new LambdaExamples.ListManipulation();
+            var list = x.RemoveWhereNotInList(new List<LambdaExamples.RemoveMe> { new LambdaExamples.RemoveMe("Nike", "Running") });
+            list.ShouldNotBeEmpty();
+            list.Count(x => x.Brand == "Nike" && x.Category == "Running").ShouldNotBe(0);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        public void GetNShoesTest(int n)
+        {
+            var x = new LambdaExamples.ListManipulation();
+            var list = x.GetTopNShoes(n);
+            list.ShouldNotBeEmpty();
+            list.Count.ShouldBe(n);
+        }
+
+    }
+}
