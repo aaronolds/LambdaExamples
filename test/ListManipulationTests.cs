c44c9ce19d8f61cadfe396f67fd9659edb7a7fd2
Aaron P. Olds <aaron.olds@gmail.com>
2019-11-11 09:36:31 -0500

    Added new examples

---
+++ test/ListManipulationTests.cs
@@ -0,0 +1,38 @@
+using Shouldly;
+using Xunit;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LambdaExamplesTests
+{
+    public class ListManipulationTests
+    {
+        [Fact]
+        public void RemoveBrandTest()
+        {
+            var x = new LambdaExamples.ListManipulation();
+            var list = x.RemoveBrand("Brooks");
+            list.ShouldNotBeEmpty();
+            list.Count(x => x.Brand == "Brooks").ShouldBe(0);
+        }
+
+        [Fact]
+        public void RemoveBrandListTest()
+        {
+            var x = new LambdaExamples.ListManipulation();
+            var list = x.RemoveWhereInList(new List<LambdaExamples.RemoveMe> { new LambdaExamples.RemoveMe("Nike", "Running") });
+            list.ShouldNotBeEmpty();
+            list.Count(x => x.Brand == "Nike" && x.Category == "Running").ShouldBe(0);
+        }
+        
+        [Fact]
+        public void RemoveBrandNotListTest()
+        {
+            var x = new LambdaExamples.ListManipulation();
+            var list = x.RemoveWhereNotInList(new List<LambdaExamples.RemoveMe> { new LambdaExamples.RemoveMe("Nike", "Running") });
+            list.ShouldNotBeEmpty();
+            list.Count(x => x.Brand == "Nike" && x.Category == "Running").ShouldNotBe(0);
+        }
+
+    }
+}
